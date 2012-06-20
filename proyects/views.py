3881029a198e57f6b110d34a20502f00ce79d72e
nelsonpinto <nelsonpinto@openautomation.cl>
2012-06-19 23:54:35 -0400

    vista modelo proyectos

--- proyects/views.py
+++ proyects/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
 from django.template import RequestContext
 
-def index(request):
-    qs = User.objects.all().order_by('username','first_name','last_name')
-    return render_to_response('proyects/index.html', locals(), context_instance=RequestContext(request))
+from proyects.models import proyect
 
+def index(request):
+    qs = proyect.objects.all().order_by('nameproyect','stateproyect','company','division','description')
+    return render_to_response('proyects/index.html', locals(), context_instance=RequestContext(request))
