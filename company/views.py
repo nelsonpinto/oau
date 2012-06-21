f423ec2653a90f66caa98fe196c14e95735151ab
nelsonpinto <nelsonpinto@openautomation.cl>
2012-06-21 00:07:10 -0400

    integracion resto de vista

--- company/views.py
+++ company/views.py
@@ -5,6 +5,8 @@ from django.template import RequestContext
 from django.views.defaults import page_not_found as default_page_not_found 
 from django.views.defaults import server_error as default_server_error
 
+from company.models import client
+
 def page_not_found(request, template='404.html'):
     return default_page_not_found(request, template=template)
 
@@ -15,5 +17,5 @@ def home(request):
     return render_to_response('home.html', locals(), context_instance=RequestContext(request))
 
 def index(request):
-    qs = User.objects.all().order_by('username','first_name','last_name')
+    qs = client.objects.all().order_by('company','nameproyect','division','description')
     return render_to_response('company/index.html', locals(), context_instance=RequestContext(request))
--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
 from django.template import RequestContext
 
+from store.models import store
+
 def index(request):
-    qs = User.objects.all().order_by('username','first_name','last_name')
+    qs = store.objects.all().order_by('titleproduct','codeproduct','productdescription','priceproduct')
     return render_to_response('store/index.html', locals(), context_instance=RequestContext(request))
